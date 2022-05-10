a4eb30da9ccc33be5d6c9e54b8a3050d4bf94fc0
WellingtonSilva12 <wellingtonsilva180g@gmail.com>
2022-05-10 16:55:18 -0300

    Aula 3

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ function onScroll() {
   }
 }
 
+ 
 function openMenu() {
   document.body.classList.add('menu-expanded')
 }
@@ -13,18 +14,3 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
-
-ScrollReveal({
-  origin: 'top',
-  distance: '30px',
-  duration: 700,
-}).reveal(`
-  #home, 
-  #home img, 
-  #home .stats, 
-  #services,
-  #services header,
-  #services .card
-  #about, 
-  #about header, 
-  #about .content`)
