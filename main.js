41f00ec7a7c1d8fb1224cded38c9c6287dfda197
WellingtonSilva12 <wellingtonsilva180g@gmail.com>
2022-05-13 17:35:13 -0300

    atualização NLW

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
+window.addEventListener('scroll', onScroll)
+onScroll()
+
 function onScroll() {
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+}
+function showNavOnScroll() {
   if (scrollY > 0) {
     navigation.classList.add('scroll')
   } else {
     navigation.classList.remove('scroll')
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 500) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
   }
 }
 
@@ -18,4 +33,14 @@ ScrollReveal({
   origin: 'top',
   distance: '30px',
   duration: '700'
-}).reveal('#home')
+}).reveal(`
+  #home, 
+  #home img, 
+  #home .stats, 
+  #services,
+  #services header,
+  #services .card,
+  #about,
+  #about header,
+  #about .content
+`)
